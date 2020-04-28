dc479014402efa5aac4a8ff0a51fe78db3a55474
Stephen <stephen.yin@outlook.com>
2020-04-28 19:18:12 +0800

    完成

---
+++ server/build/precache-manifest.d2760d181fe4c50d935a3641f4047483.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfd6c4ce6af73d782c28ba029522001f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2387e9a01f988f948b33",
+    "url": "/static/css/main.cc6ebcb9.chunk.css"
+  },
+  {
+    "revision": "b7ead2c7aaa44036d484",
+    "url": "/static/js/2.41ca82e9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.41ca82e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2387e9a01f988f948b33",
+    "url": "/static/js/main.771c8311.chunk.js"
+  },
+  {
+    "revision": "4769b01184c635b2d917",
+    "url": "/static/js/runtime-main.aa4083ec.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- server/src/index.js
+++ server/src/index.js
@@ -5,11 +5,16 @@ const logout = require('./routes/logout')
 const users = require('./routes/users')
 const bodyParser = require('body-parser')
 const dotenv = require('dotenv')
+const path = require('path')
 
 // .env 配置
 dotenv.config()
 
 const app = express()
+
+// 静态文件
+app.use(express.static(path.join(__dirname, '../', 'build')))
+
 
 app.use(bodyParser.json())
 // 路由
@@ -19,7 +24,7 @@ app.use('/api/logout', logout)
 app.use('/api/users', users)
 
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.sendFile(path.join(__dirname, '../', 'build', 'index.html'))
 })
 
 app.listen(5000, () => console.log('server starts at port 5000'))
